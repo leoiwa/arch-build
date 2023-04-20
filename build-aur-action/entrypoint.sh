c4b15aaa8e48b92206b135eac3c723bfc2a3fb16
leoiwa <232403237@qq.com>
2023-04-20 23:39:09 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -2,16 +2,8 @@
 
 pkgname=$1
 
-sudo bash -c 'cat << EOM >> /etc/pacman.conf
-[archlinuxcn]
-Server = https://repo.archlinuxcn.org/x86_64
-EOM'
-
-sudo chmod a+rw -R ./
-
-sudo pacman-key --init
-sudo pacman -Sy --noconfirm && sudo pacman -S --noconfirm archlinuxcn-keyring
-sudo pacman -S --noconfirm yay
-
-
-yay -Syu --needed --noconfirm --builddir=./ "$pkgname"
+useradd builder -m
+echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+cd /builder
+su - builder
+yay -S --noconfirm --builddir=./ "$pkgname"
