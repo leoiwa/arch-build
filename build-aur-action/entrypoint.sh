0b4eb6369d1bebd92793210ef1698ef8d909ef7e
leoiwa <232403237@qq.com>
2023-04-20 19:48:03 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -14,4 +14,4 @@ sudo pacman -Sy --noconfirm && sudo pacman -S --noconfirm archlinuxcn-keyring
 sudo pacman -S --noconfirm yay
 
 
-yay -Syu --noconfirm --builddir=./ "$pkgname"
+yay -Syu --needed --noconfirm --builddir=./ "$pkgname"
