e8dcc13bd6d412c831f93aeebe436ff821617029
leoiwa <232403237@qq.com>
2023-04-20 18:47:26 +0800

    Update entrypoint.sh
    调整脚本执行方式。

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -2,17 +2,13 @@
 
 pkgname=$1
 
-useradd builder -m
-echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-chmod -R a+rw .
-
 cat << EOM >> /etc/pacman.conf
 [archlinuxcn]
 Server = https://repo.archlinuxcn.org/x86_64
 EOM
 
-pacman-key --init
-pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
-pacman -S --noconfirm yay
+sudo pacman-key --init
+sudo pacman -Sy --noconfirm && sudo pacman -S --noconfirm archlinuxcn-keyring
+sudo pacman -S --noconfirm yay
 
-sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
+yay -S --noconfirm --builddir=./ "$pkgname"
